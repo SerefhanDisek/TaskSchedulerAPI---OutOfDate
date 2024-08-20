f089c9e999ceec07032d61c6846b4e0ccbc9dc94
seref <seref@DESKTOP-9S2EU5A>
2024-08-20 16:28:20 +0300

    TaskController2

--- TaskSchedulerAPI.Core/DTOs/TaskUpdateDto.cs
+++ TaskSchedulerAPI.Core/DTOs/TaskUpdateDto.cs
@@ -8,5 +8,8 @@ namespace TaskSchedulerAPI.Core.DTOs
 {
     public class TaskUpdateDto
     {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
     }
 }
